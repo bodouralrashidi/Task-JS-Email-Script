6baacdc3a1103cc848b21a64a75988f3d2d4fad6
Bodour <50752964+bodouralrashidi@users.noreply.github.com>
2022-04-22 16:03:13 +0300

    error

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+const excelToJson = require('convert-excel-to-json');
+ 
+const result = excelToJson({
+    sourceFile: 'names.xlsx'
+});
+ console.log(result)
+
+
+ // using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'bodouralrashidi@gmail.com', // Change to your recipient
+  from: 'bodouralrashidi@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
